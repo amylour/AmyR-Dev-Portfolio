6350f651c922944f21811d2d70be13385e7d799b
Amy Richardson <105483927+amylour@users.noreply.github.com>
2024-01-01 20:10:12 +0000

    update: header with links, mobile nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,29 @@
-import ThemeToggler from "./ThemeToggler"
+'use client';
+import React, { useState, useEffect } from 'react';
+
+// components
+import Logo from './Logo';
+import ThemeToggler from "./ThemeToggler";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header>
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <Logo />
+          <div className='flex items-center gap-x-6'>
+          {/* nav */}
+          <Nav containerStyles='hidden xl:flex gap-x-8 items-center' />
       <ThemeToggler />
+      {/* mobile nav */}
+      <div className='xl:hidden'>
+        <MobileNav />
+      </div>
+      </div>
+      </div>
+      </div>
     </header>
   );
 };
