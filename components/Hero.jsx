e3c2721ae08a956663e8d1b2741c36407e7ca1d0
Amy Richardson <105483927+amylour@users.noreply.github.com>
2024-01-04 15:54:42 +0000

    add: components

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,11 +3,38 @@ import { Button } from "./ui/Button";
 import { Download, Send } from "lucide-react";
 
 import {
+  RiBriefcase4Fill,
+  RiTeamFill,
+  RiTodoFill,
+  RiArrowDownSLine,
+} from 'react-icons/ri';
 
-} from 'react'
+// components
+import DevImg from "./DevImg";
+import Badge from "./Badge";
+import Socials from "./Socials";
 
 const Hero = () => {
-  return <div>Hero</div>
+  return (
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-8">
+        {/* text */}
+        <div>
+            <div>Full Stack Developer</div>
+            <h1>Web Developer, Graphic Designer</h1>
+            <p>words and stuff</p>
+        </div>
+        {/* image */}
+        <div>image</div> 
+        </div>
+        {/* icons */}
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary" />
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Hero;
