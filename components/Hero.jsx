c394d944bb2c42432dcfa08cb2f42ae9a2de839a
Amy Richardson <105483927+amylour@users.noreply.github.com>
2024-02-17 20:39:23 +0000

    add: values badges

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -45,9 +45,24 @@ const Hero = () => {
         <div className='hidden xl:flex relative'>
           {/* badge 1 */}
           <Badge 
+          containerStyles='absolute top-[24%] -left-[5rem]'
             icon={<RiBriefcase4Fill />} 
-            endCountNum={3} 
+            endCountNum={2} 
             badgeText='Years Of Experience' 
+          />
+          {/* badge 2 */}
+          <Badge 
+          containerStyles='absolute top-[80%] -left-[1rem]'
+            icon={<RiTodoFill />} 
+            endCountNum={6} 
+            badgeText='Finished Projects' 
+          />
+          {/* badge 3 */}
+          <Badge 
+          containerStyles='absolute top-[55%] -right-8'
+            icon={<RiBriefcase4Fill />} 
+            endCountNum={10} 
+            badgeText='Programming Skills' 
           />
             <div className='bg-hero_shape dark:bg-hero_shape2_light w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2'></div>
             <DevImg containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom' imgSrc='/hero/falcon.png'/>
